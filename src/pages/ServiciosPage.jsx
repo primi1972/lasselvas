efa2f788d1cc9eb8c576224254566c06dd604e78
mecnet <mexworks72@gmail.com>
2022-11-30 19:18:07 -0600

    actualizado3

--- src/pages/ServiciosPage.jsx
+++ src/pages/ServiciosPage.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import Banner1 from "../assets/Servicios/bg.jpg";
 import CardGeneral from "../components/cards";
 
@@ -7,11 +8,23 @@ const Servicios = () => {
       <img src={Banner1} className="banner1" alt="" />
       <section className="d-flex justify-content-center maxwidth align-items-start flex-wrap py-5 mx-auto">
         <div className="titulo w-100">
-          <h1 className="text-success text-uppercase text-center">Servicios</h1>
-          <h5 className="text-center fw-light my-3 text-white">
+          <motion.h1
+            initial={{ x: 150, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.4, duration: 1 }}
+            className="text-success text-uppercase text-center"
+          >
+            Servicios
+          </motion.h1>
+          <motion.h5
+            initial={{ x: -150, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.4, duration: 1 }}
+            className="text-center fw-light my-3 text-white"
+          >
             Fletes, maquilado, estufado, instalacion y todo lo que necesites lo
             encuentras con nosotros.
-          </h5>
+          </motion.h5>
         </div>
         <div className="text-white d-flex w-90 my-5 gap-4 justify-content-center flex-wrap">
           <CardGeneral
@@ -23,12 +36,12 @@ const Servicios = () => {
             imagen="fa-solid fa-temperature-high"
             titulo="Material Estufado"
             texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint adipisci, dolore voluptates minus natus iusto voluptas velsaepe error rerum ex ad, eaque necessitatibus neque numquam eiusaccusamus rem. In suscipit earum placeat quibusdam teneturvoluptates ea ab cum eius odio est fugiat perspiciatis liberoculpa, saepe deserunt consequuntur."
-          ></CardGeneral>     
+          ></CardGeneral>
           <CardGeneral
             imagen="fa-solid fa-store"
             titulo="Abasto seguro y continuo"
             texto="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero sint adipisci, dolore voluptates minus natus iusto voluptas velsaepe error rerum ex ad, eaque necessitatibus neque numquam eiusaccusamus rem. In suscipit earum placeat quibusdam teneturvoluptates ea ab cum eius odio est fugiat perspiciatis liberoculpa, saepe deserunt consequuntur."
-          ></CardGeneral>                  
+          ></CardGeneral>
 
           <CardGeneral
             imagen="fa-solid fa-clipboard"
