9996245b79d157d5da67f8c205bc30235f669e27
unknown <mexworks72@gmail.com>
2022-12-05 13:36:24 -0600

    respaldo3

--- src/components/TextoHistoria.jsx
+++ src/components/TextoHistoria.jsx
@@ -1,19 +1,22 @@
 import { motion } from "framer-motion";
+import imagenFondo from "../assets/historia/tronco.jpg";
+import "../styles/historia.css";
 
-const textoHistoria = ({titulo, texto, imagen}) => {
-    return(
-        <motion.div
-        initial={{ y: 50, opacity: 0 }}
-        whileInView={{ y: [0, 20, 0], opacity: 1 }}
-        transition={{ delay: 0.2, duration: 2 }}
-        className="d-flex justify-content-between align-items-center minwidth py-5"
-      >
-        <div className="texto w-50">
-          <h3 className="text-center">{titulo}</h3>
-          <p className="align-justify">{texto}</p>
-        </div>
-        <img src={imagen} alt="" style={{width: "400px"}}/>
-      </motion.div>
-    )    
-}
+const textoHistoria = ({ titulo, texto, imagen }) => {
+  return (
+    <motion.div
+      initial={{ y: 50, opacity: 0 }}
+      whileInView={{ y: [0, 20, 0], opacity: 1 }}
+      transition={{ delay: 0.2, duration: 2 }}
+      className="texto-historia d-flex justify-content-center gap-2 align-items-center minwidth py-5"
+    >
+      <div className="texto w-75 text-white">
+        <img src={imagenFondo} alt="" />
+        <h3 className="text-center px-5 py-3 shadow">{titulo}</h3>
+        <p className="align-justify px-5 py-3 shadow">{texto}</p>
+      </div>
+      <img src={imagen} alt="" style={{ width: "400px" }} className="w-25"/>
+    </motion.div>
+  );
+};
 export default textoHistoria;
